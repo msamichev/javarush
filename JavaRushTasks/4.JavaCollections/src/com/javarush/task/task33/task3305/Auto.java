323d607be805ee7f4603bca999e9988a79f704d5
msamichev <msamichev@at-consulting.ru>
2017-03-13 13:12:31 +0300

    +1

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task33/task3305/Auto.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task33/task3305/Auto.java
@@ -3,11 +3,11 @@ package com.javarush.task.task33.task3305;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property="className")
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, property = "className")
 @JsonSubTypes({
-        @JsonSubTypes.Type(value=Car.class),
-        @JsonSubTypes.Type(value=Motorbike.class),
-        @JsonSubTypes.Type(value=RaceBike.class)
+        @JsonSubTypes.Type(value = Car.class, name = "car"),
+        @JsonSubTypes.Type(value = Motorbike.class, name = "motorbike"),
+        @JsonSubTypes.Type(value = RaceBike.class, name = "race-bike")
 })
 public abstract class Auto {
     protected String name;
--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task38/task3802/VeryComplexClass.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task38/task3802/VeryComplexClass.java
@@ -4,9 +4,13 @@ package com.javarush.task.task38.task3802;
 Проверяемые исключения (checked exception)
 */
 
+
+import java.io.FileInputStream;
+
 public class VeryComplexClass {
     public void veryComplexMethod() throws Exception {
-        Integer.parseInt("A");
+
+        new FileInputStream("abcd");
     }
 
     public static void main(String[] args) {
