a10a77e50833904a59449d65a328e3c56e815be4
msamichev <msamichev@at-consulting.ru>
2017-03-15 15:44:56 +0300

    +1

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task33/task3303/Solution.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task33/task3303/Solution.java
@@ -3,6 +3,7 @@ package com.javarush.task.task33.task3303;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 /* 
@@ -11,7 +12,8 @@ import java.io.IOException;
 public class Solution {
     public static <T> T convertFromJsonToNormal(String fileName, Class<T> clazz) throws IOException {
         ObjectMapper om = new ObjectMapper();
-        return om.readValue(new File(fileName), clazz);
+        return om.readValue(new FileReader(new File(fileName)), clazz);
+
     }
 
     public static void main(String[] args) {
