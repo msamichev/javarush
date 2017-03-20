faeb02348add4ac0caad030f2bcae767aa380deb
msamichev <msamichev@at-consulting.ru>
2017-03-20 13:56:44 +0300

    task15

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task34/task3410/model/Model.java
@@ -15,33 +15,126 @@ public class Model {
     private LevelLoader levelLoader = new LevelLoader(Paths.get("C:\\prj\\JavaRush\\JavaRushTasks\\4.JavaCollections\\src\\com\\javarush\\task\\task34\\task3410\\res\\levels.txt"));
 
 
-    public void setEventListener(EventListener eventListener)
-    {
+    public void setEventListener(EventListener eventListener) {
         this.eventListener = eventListener;
     }
 
 
-    public GameObjects getGameObjects()
-    {
+    public GameObjects getGameObjects() {
         return gameObjects;
     }
 
-    public void restartLevel(int level)
-    {
+    public void restartLevel(int level) {
         this.gameObjects = levelLoader.getLevel(level);
     }
 
-    public void restart()
-    {
+    public void restart() {
         restartLevel(currentLevel);
     }
 
-    public void startNextLevel()
-    {
+    public void startNextLevel() {
         restartLevel(++currentLevel);
     }
 
-    public void move(Direction direction) {
+    public void move(Direction direction)
+    {
+        Player player = gameObjects.getPlayer();
 
+        if (checkWallCollision(player, direction))
+        {
+            return;
+        }
+        if (checkBoxCollisionAndMoveIfAvaliable(direction))
+        {
+            return;
+        }
+
+        int sellSize = FIELD_SELL_SIZE;
+        switch (direction)
+        {
+            case LEFT:
+                player.move(-sellSize, 0);
+                break;
+            case RIGHT:
+                player.move(sellSize, 0);
+                break;
+            case UP:
+                player.move(0, -sellSize);
+                break;
+            case DOWN:
+                player.move(0, sellSize);
+        }
+        checkCompletion();
     }
+
+    public boolean checkWallCollision(CollisionObject gameObject, Direction direction) {
+        for (Wall wall : gameObjects.getWalls()) {
+            if (gameObject.isCollision(wall, direction)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkBoxCollisionAndMoveIfAvaliable(Direction direction) {
+
+        Player player = gameObjects.getPlayer();
+
+        GameObject stoped = null;
+        for (GameObject gameObject : gameObjects.getAll()) {
+            if (!(gameObject instanceof Player) && !(gameObject instanceof Home) && player.isCollision(gameObject, direction)) {
+                stoped = gameObject;
+            }
+        }
+
+        if ((stoped == null)) {
+            return false;
+        }
+        if (stoped instanceof Box) {
+            Box stopedBox = (Box) stoped;
+            if (checkWallCollision(stopedBox, direction)) {
+                return true;
+            }
+            for (Box box : gameObjects.getBoxes()) {
+                if (stopedBox.isCollision(box, direction)) {
+                    return true;
+                }
+            }
+            switch (direction) {
+                case LEFT:
+                    stopedBox.move(-FIELD_SELL_SIZE, 0);
+                    break;
+                case RIGHT:
+                    stopedBox.move(FIELD_SELL_SIZE, 0);
+                    break;
+                case UP:
+                    stopedBox.move(0, -FIELD_SELL_SIZE);
+                    break;
+                case DOWN:
+                    stopedBox.move(0, FIELD_SELL_SIZE);
+            }
+        }
+        return false;
+    }
+
+    public void checkCompletion()
+    {
+        boolean yes = true;
+
+        for (Home home : gameObjects.getHomes())
+        {
+            boolean currentyes = false;
+
+            for (Box box : gameObjects.getBoxes())
+            {
+                if ((box.getX() == home.getX()) && (box.getY() == home.getY()))
+                    currentyes = true;
+            }
+
+            if (!currentyes) yes = false;
+        }
+        if (yes)
+            eventListener.levelCompleted(currentLevel);
+    }
+
 }
